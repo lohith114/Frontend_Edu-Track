185cbecce161ec034388ccdc466d714a4369ae3c
lohith114 <111067381+lohith114@users.noreply.github.com>
2024-12-17 06:55:16 +0530

    Update Firebase.js

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,16 +1,17 @@
-// src/firebase/firebase.js
+// src/Firebase/Firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBSG8-KlzDTHHNkvLS6ioPrD_P1XK9KrP4",
-    authDomain: "lmkscloud.firebaseapp.com",
-    projectId: "lmkscloud",
-    storageBucket: "lmkscloud.appspot.com",
-    messagingSenderId: "626221629609",
-    appId: "1:626221629609:web:65371b5d41260110eee9b8",
-    measurementId: "G-631QGYG5F1"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
